13df005ead2d24b46cb3d4c5b42d2e8937b8125b
scooter7 <scooter.vineburgh@gmail.com>
2023-11-22 23:51:50 -0600

    Update topicmodeling.py

--- topicmodeling.py
+++ topicmodeling.py
@@ -1,22 +1,23 @@
 import streamlit as st
 from scholarly import scholarly
 import pandas as pd
-import networkx as nx
-from pyvis.network import Network
-import tempfile
+import plotly.graph_objects as go
 import time
 import logging
 
-def fetch_papers(topic, max_results=10, retry_limit=3, retry_delay=5):
+def fetch_papers(topic, max_results=10, min_citations=0, start_year=None, end_year=None, retry_limit=3, retry_delay=5):
     search_query = scholarly.search_pubs(topic)
     papers = []
     retry_count = 0
 
-    # Fetch more papers initially
-    while len(papers) < max_results * 2 and retry_count < retry_limit:
+    while len(papers) < max_results and retry_count < retry_limit:
         try:
             paper = next(search_query)
-            papers.append(paper)
+            year = int(paper.bib.get('year', 0))
+            citedby = paper.bib.get('citedby', 0)
+
+            if citedby >= min_citations and (start_year is None or year >= start_year) and (end_year is None or year <= end_year):
+                papers.append(paper)
         except StopIteration:
             break
         except Exception as e:
@@ -24,48 +25,53 @@ def fetch_papers(topic, max_results=10, retry_limit=3, retry_delay=5):
             retry_count += 1
             time.sleep(retry_delay)
 
-    # Sort papers by citation count and select the top results
-    papers.sort(key=lambda x: x.get('citedby', 0), reverse=True)
-    return papers[:max_results]
+    return papers
 
-def create_graph(papers):
-    G = nx.Graph()
-    for paper in papers:
-        # Check if 'title' and 'author' are in the paper dictionary
-        if 'title' in paper and 'author' in paper:
-            paper_title = paper['title']
-            G.add_node(paper_title, title=paper_title, size=paper.get('citedby', 10))
-            authors = paper['author'].split(' and ')
-            for author in authors:
-                G.add_node(author, title=author, size=20)
-                G.add_edge(paper_title, author)
-    return G
+def create_plotly_graph(papers):
+    edge_x = []
+    edge_y = []
+    node_x = []
+    node_y = []
+    node_text = []
 
-def show_graph(G):
-    net = Network(height='500px', width='100%', bgcolor='#222222', font_color='white')
-    net.from_nx(G)
-    
-    # Save the graph to a temporary file and return its path
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as tmp_file:
-        net.save_graph(tmp_file.name)
-        return tmp_file.name
+    for i, paper in enumerate(papers):
+        node_x.append(i)
+        node_y.append(i)
+        node_text.append(paper.bib['title'])
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers+text',
+        text=node_text,
+        marker=dict(size=10, line_width=2))
+
+    fig = go.Figure(data=[node_trace],
+                    layout=go.Layout(
+                        showlegend=False,
+                        hovermode='closest',
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+
+    return fig
 
 def main():
     st.title("Scholarly Topic Modeling")
 
     topic = st.text_input("Enter a Topic", "Machine Learning")
     max_results = st.slider("Max number of results", 5, 50, 10)
+    min_citations = st.slider("Minimum citations", 0, 100, 10)
+    start_year = st.number_input("Start Year", min_value=1900, max_value=2023, value=2000)
+    end_year = st.number_input("End Year", min_value=1900, max_value=2023, value=2023)
 
     if topic:
-        papers = fetch_papers(topic, max_results)
+        papers = fetch_papers(topic, max_results, min_citations, start_year, end_year)
         if papers:
-            G = create_graph(papers)
-            graph_html_file = show_graph(G)
-            st.components.v1.html(open(graph_html_file, 'r', encoding='utf-8').read(), height=600)
+            fig = create_plotly_graph(papers)
+            st.plotly_chart(fig, use_container_width=True)
 
-            # Updated section for constructing the data table
-            data = [{'Title': paper['bib']['title'], 'Authors': paper['bib'].get('author', 'N/A'), 
-                     'Year': paper['bib'].get('year', 'N/A'), 'Citations': paper['bib'].get('citedby', 0)} 
+            data = [{'Title': paper.bib['title'], 'Authors': paper.bib.get('author', 'N/A'), 
+                     'Year': paper.bib.get('year', 'N/A'), 'Citations': paper.bib.get('citedby', 0)} 
                     for paper in papers]
             df = pd.DataFrame(data)
             st.write(df)
