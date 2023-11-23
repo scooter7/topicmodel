d01662f966e8d3aa3b21974e7e5ce3638f92ba2e
scooter7 <scooter.vineburgh@gmail.com>
2023-11-23 00:31:26 -0600

    Update topicmodeling.py

--- topicmodeling.py
+++ topicmodeling.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import plotly.graph_objects as go
+import time
 
 def fetch_semantic_scholar_papers(topic, max_results=10, min_citations=0, start_year=None, end_year=None, retries=3):
     url = f"https://api.semanticscholar.org/graph/v1/paper/search"
